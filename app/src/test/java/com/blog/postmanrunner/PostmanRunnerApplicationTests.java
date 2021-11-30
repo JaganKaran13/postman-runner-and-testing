91adeb513ecb60c116ebebcbe22ecca0844c2e3c
Jagan Karan <32727177+JaganKaran13@users.noreply.github.com>
2021-11-30 18:39:31 +0530

    Boiler plate

---
+++ app/src/test/java/com/blog/postmanrunner/PostmanRunnerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.postmanrunner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostmanRunnerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
